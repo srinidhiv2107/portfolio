da32aa7b41a0c1e87972c8704de24d1187a36601
Srinidhi V <srinidhi01nov@gmail.com>
2026-02-22 13:29:20 +0000

    configure custom domain deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/portfolio/",
+  base: "/",
   plugins: [react()],
   server: {
     open: true,
